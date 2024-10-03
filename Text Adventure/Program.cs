f3f88569ebef4b59469dd70acf51c561461f0205
MUKILTEO\1554910 <Jordan.isaacson07@gmail.com>
2024-10-03 13:21:30 -0700

    Start

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        public int Tokens = 3;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\tWelcome to the game of doors!");
+            Thread.Sleep(2500);
+            Console.WriteLine("\tin this game you will choose from a series of doors");
+            Thread.Sleep(4500);
+            Console.WriteLine("\tyou will choose from these doors to enter.");
+            Thread.Sleep(2500);
+            Console.WriteLine("\tBut you should be very carefull as these doors will also contain your fate");
+            Thread.Sleep(4500);
+            Console.WriteLine("\tNow you must be carefull as anything can be behind these doors and some may cost you your life");
+            Thread.Sleep(5500);
+            Console.WriteLine("\tthrought the game you will have 3 tokens that will tell you where these doors lead and whats behind them");
+            Thread.Sleep(5500);
+            Console.WriteLine("\t you have to Be carefull as you will only have 3 of these tokens so don't use them too- \r\tearly, but if you save them for to late it may lead to your death");
+            Thread.Sleep(9000);
+            Console.WriteLine("\t\t\tNOW LET THE GAMES BEGIN!!!");
+            Thread.Sleep(2000);
         }
+       
+        
+            
+        
     }
 }
